d2ca1263b215ebbdd1302840b01048a6f0f7e04a
xjahic <jahic.jakub@gmail.com>
2022-01-05 17:22:26 +0100

    first-step

---
+++ main.py
@@ -0,0 +1,18 @@
+INTRO_TEXT = "Práve si zapol hru Arkadius, v ktorej budeš bojovať proti príšerám a pri tom si zlepšovať svojho hrdinu. Si na to pripravený?" \
+             "\n 0 - Nie, bojím sa. \n 1 - Áno, poďme na to."
+
+continue_game = True
+while continue_game:
+
+    # intro
+    print(INTRO_TEXT)
+    intro_choice = input("Aká je tvoja voľba?: ")
+    if intro_choice not in ["0", "1"]:
+        print("Netrafil si ani jednu možnú voľbu. Musím sa ťa spýtať ešte raz.")
+        continue
+
+    if intro_choice == "0":
+        print("To ma mrzí. Dúfam, že prídeš aspoň neskôr.")
+        break
+
+    print("Výborne, máš odvahu. To sa mi páči.")
