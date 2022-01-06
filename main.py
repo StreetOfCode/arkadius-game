a423a9af90d51b359316efb93d9e151075ba4372
xjahic <jahic.jakub@gmail.com>
2022-01-06 11:34:45 +0100

    fourth-step

--- main.py
+++ main.py
@@ -28,6 +28,21 @@ abilities = {
     }
 }
 
+
+def print_abilities_options():
+    for i, ability in enumerate(abilities.keys()):
+        ability_option = str(i + 1) + ' - ' + ability
+        if ability == "Život":
+            ability_option += " " + "- jeden bod pridá 5 života"
+        print(ability_option)
+
+
+def print_abilities_points():
+    for k, v in abilities.items():
+        print(k + " - " + str(v["points"]), end=", ")
+    print("\n")
+
+
 continue_game = True
 while continue_game:
 
@@ -44,6 +59,7 @@ while continue_game:
 
     print("Výborne, máš odvahu. To sa mi páči.")
 
+    # name
     name_picked = False
     hero_name = ""
     while not name_picked:
@@ -61,3 +77,39 @@ while continue_game:
             break
 
     print("Ahoj", hero_name)
+
+    # points
+    print(hero_name + ", Tvoje schopnosti sú momentálne na tom takto:")
+    print_abilities_points()
+    print("Máš 7 bodov, ktore si rozdeľ naprieč schopnostiam podla svojich preferencií.")
+    abilities_picked = False
+    abilities_picked_count = 0
+    while not abilities_picked:
+        print_abilities_options()
+
+        option = input(
+            "Máš " + str(7 - abilities_picked_count) + " možnosti na zlepšenie. Ktorú schopnosť chceš vylepšiť?: ")
+
+        if option.isnumeric() and int(option) in list(range(1, len(abilities) + 1)):
+            chosen_ability_name = list(abilities.keys())[int(option) - 1]
+            chosen_ability = abilities[chosen_ability_name]
+            print("\nVybral si si schopnosť - " + chosen_ability_name + ". Pridávam ti bod.")
+            if chosen_ability_name == 'Život':
+                chosen_ability["points"] += 5
+            else:
+                chosen_ability["points"] += 1
+            print("Teraz tvoje body vyzerajú takto: ")
+            print_abilities_points()
+        else:
+            print("Netrafil si výber, musím sa ťa spýtať ešte raz. \n")
+            continue
+
+        abilities_picked_count += 1
+        if abilities_picked_count == 7:
+            abilities_picked = True
+
+    print(
+        "Výborne " + hero_name + ". Dokončil si pridavánie schopnosti. Pre rekapituláciu, teraz vyzeraju tvoje schopnosti takto.")
+    print_abilities_points()
+
+    print("Si pripravený na prvý súboj?")
