9da2e7a00e9e4326216f2baa8e6b8ec27e3ac519
xjahic <jahic.jakub@gmail.com>
2022-01-06 11:21:59 +0100

    third-step

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 INTRO_TEXT = "Práve si zapol hru Arkadius, v ktorej budeš bojovať proti príšerám a pri tom si zlepšovať svojho hrdinu. Si na to pripravený?" \
              "\n 0 - Nie, bojím sa. \n 1 - Áno, poďme na to."
 
+abilities = {
+    "Útočná sila": {
+        "points": 1,
+        "description": "Sila je potrebna k útoku, do ktorého okrem sily vstupuje aj obratnosť a skill."
+    },
+    "Obrana": {
+        "points": 1,
+        "description": "Celkový obrana sa ráta z bodov obrany + obratnosti."
+    },
+    "Obratnosť": {
+        "points": 1,
+        "description": "Obratnosť je dôležitá aj pre obranu aj pre útok."
+    },
+    "Skill": {
+        "points": 1,
+        "description": "SKill je dôležitý pri normálnom útoku ako aj kritickom útoku"
+    },
+    "Život": {
+        "points": 50,
+        "description": "Život je dôležitý pri bitke. Život sa dá doplniť po každom súboji."
+    },
+    "Šťastie": {
+        "points": 1,
+        "description": "Šťastie je dôležité pre kritický útok"
+    }
+}
+
 continue_game = True
 while continue_game:
 
