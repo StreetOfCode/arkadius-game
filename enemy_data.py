c0b91820b657f4c75588da1afc619dd6872ed46f
xjahic <jahic.jakub@gmail.com>
2022-01-06 16:16:26 +0100

    eleventh-step

---
+++ enemy_data.py
@@ -0,0 +1,47 @@
+enemies = {
+    1: {
+        "name": "Harpya (LVL 1)",
+        "Útočná sila": 3,
+        "Obrana": 1,
+        "Obratnosť": 2,
+        "Skill": 2,
+        "Šťastie": 1,
+        "Život": 25,
+    },
+    2: {
+        "name": "Golem (LVL 2)",
+        "Útočná sila": 4,
+        "Obrana": 3,
+        "Obratnosť": 1,
+        "Skill": 2,
+        "Šťastie": 1,
+        "Život": 35,
+    },
+    3: {
+        "name": "Mocny demon (LVL 3)",
+        "Útočná sila": 4,
+        "Obrana": 4,
+        "Obratnosť": 2,
+        "Skill": 3,
+        "Šťastie": 5,
+        "Život": 50,
+    },
+    4: {
+        "name": "Meduza (LVL 4)",
+        "Útočná sila": 7,
+        "Obrana": 7,
+        "Obratnosť": 5,
+        "Skill": 5,
+        "Šťastie": 1,
+        "Život": 40,
+    },
+    5: {
+        "name": "KRAKEN - Boh temnoty",
+        "Útočná sila": 7,
+        "Obrana": 7,
+        "Obratnosť": 7,
+        "Skill": 7,
+        "Šťastie": 7,
+        "Život": 80,
+    }
+}
